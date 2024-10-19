3fca073e3f7952576c121baf7e5a6e416185ac9f
Thomas Li-Smith <geekcentric@Thomass-MacBook-Pro-2.local>
2024-10-19 12:38:02 -0700

    confirm creating new item works

--- src/pages/NewItem/useNewItemData.ts
+++ src/pages/NewItem/useNewItemData.ts
@@ -1,9 +1,37 @@
+import useRadNavigation from '../../hooks/useRadNavigation.ts';
+
 const useNewItemData = () => {
   const params = new URLSearchParams(window.location.search);
   const parentId = params.get('parentId');
+  const { navigateRaw } = useRadNavigation();
 
-  const createNewItem = () => {
-    console.log('Parent ID ==>', parentId);
+  const createNewItem = async () => {
+    const newItem = {
+      title: 'New Item',
+      isPublic: false,
+      parentId: parentId || '',
+    };
+
+    try {
+      const response = await fetch('/api/item', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('accessToken') || '',
+        },
+        body: JSON.stringify(newItem),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const goToAddress = parentId ? `/item/${parentId}` : '/home';
+
+      navigateRaw(goToAddress);
+    } catch (err) {
+      console.log('this is the error ==>', err);
+    }
   };
 
   return { parentId, createNewItem };
