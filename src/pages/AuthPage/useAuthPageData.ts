ac2a6c4f73c28ca4ff0a601c39b428900a142ab9
Thomas Li-Smith <geekcentric@Thomass-MacBook-Pro-2.local>
2024-10-19 21:30:28 -0700

    get rid of local storage set

--- src/pages/AuthPage/useAuthPageData.ts
+++ src/pages/AuthPage/useAuthPageData.ts
@@ -4,8 +4,6 @@ const useAuthPageData = () => {
     const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
     const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user:email`;
 
-    window.localStorage.setItem('redirectUri', url);
-
     window.location.href = url;
   };
 
