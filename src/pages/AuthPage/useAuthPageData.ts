00c3249ae6458f18c905c7ba7eacb90ed295bb13
Thomas Li-Smith <geekcentric@Thomass-MacBook-Pro-2.local>
2024-10-19 20:54:57 -0700

    move redirect ui to env variable

--- src/pages/AuthPage/useAuthPageData.ts
+++ src/pages/AuthPage/useAuthPageData.ts
@@ -1,10 +1,10 @@
 const useAuthPageData = () => {
   const redirectToGithub = () => {
-    const redirectUri = "http://localhost:5173/auth/sso-redirect";
+    const redirectUri = import.meta.env.VITE_REDIRECT_URI;
     const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
     const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user:email`;
 
-    window.localStorage.setItem("redirectUri", url);
+    window.localStorage.setItem('redirectUri', url);
 
     window.location.href = url;
   };
